c4285d60f6e02fb36a22def507dbbf51ce6b9ff5
Vanessa Pereira <vnspereira11@gmail.com>
2023-07-24 23:48:11 -0300

    Update MealsController

--- src/controllers/MealsController.js
+++ src/controllers/MealsController.js
@@ -4,7 +4,8 @@ const AppError = require("../utils/AppError");
 
 class MealsController {
   async create(request, response) {
-    return response.status(201).json();
+    const { name, category, price, description } = request.body;
+    return response.status(201).json({ name, category, price, description });
   }
 
   async update(request, response) {
