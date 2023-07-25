32180ad48935747c8490a5ff411738501c0c7d00
Vanessa Pereira <vnspereira11@gmail.com>
2023-07-25 18:18:22 -0300

    Update MealsController: create method

--- src/controllers/MealsController.js
+++ src/controllers/MealsController.js
@@ -4,8 +4,37 @@ const AppError = require("../utils/AppError");
 
 class MealsController {
   async create(request, response) {
-    const { name, category, price, description } = request.body;
-    return response.status(201).json({ name, category, price, description });
+    const { name, category, ingredients, price, description } = request.body;
+    const { user_id } = request.params;
+
+    const user = await knex("users").where({ id: user_id }).first();
+
+    if (user.admin === 0) {
+      throw new AppError("Sem permissão para cadastrar pratos.");
+    }
+
+    if (!name || !category || !price || !description) {
+      throw new AppError("Preencha os campos solicitados.");
+    }
+
+    const [meal_id] = await knex("meals").insert({
+      name,
+      category,
+      price,
+      description,
+      user_id,
+    });
+
+    const ingredientsInsert = ingredients.map((name) => {
+      return {
+        meal_id,
+        name,
+      };
+    });
+
+    await knex("ingredients").insert(ingredientsInsert);
+
+    return response.status(201).json();
   }
 
   async update(request, response) {
--- src/routes/meals.routes.js
+++ src/routes/meals.routes.js
@@ -6,7 +6,7 @@ const mealsRoutes = Router();
 
 const mealsController = new MealsController();
 
-mealsRoutes.post("/", mealsController.create);
+mealsRoutes.post("/:user_id", mealsController.create);
 mealsRoutes.put("/edit/:id", mealsController.update);
 
 module.exports = mealsRoutes;
