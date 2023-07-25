37312e299b5813eb02d00366d021fe1941f022a0
Vanessa Pereira <vnspereira11@gmail.com>
2023-07-24 23:37:19 -0300

    Create meals migration

---
+++ src/database/knex/migrations/20230725022139_createMeals.js
@@ -0,0 +1,14 @@
+exports.up = (knex) =>
+  knex.schema.createTable("meals", (table) => {
+    table.increments("id").primary();
+    table.integer("user_id").references("id").inTable("users");
+    table.text("image");
+    table.text("name").notNullable();
+    table.text("category").notNullable();
+    table.float("price").notNullable();
+    table.text("description").notNullable();
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+  });
+
+exports.down = (knex) => knex.schema.dropTable("meals");
