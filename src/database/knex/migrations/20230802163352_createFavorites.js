c95cb8570365b7adcbf94a691becb46302edd67d
Vanessa Pereira <vnspereira11@gmail.com>
2023-08-02 13:49:17 -0300

    Create favorites migration

---
+++ src/database/knex/migrations/20230802163352_createFavorites.js
@@ -0,0 +1,8 @@
+exports.up = (knex) =>
+  knex.schema.createTable("favorites", (table) => {
+    table.increments("id").primary();
+    table.integer("user_id").references("id").inTable("users");
+    table.integer("meal_id").references("id").inTable("meals");
+  });
+
+exports.down = (knex) => knex.schema.dropTable("favorites");
