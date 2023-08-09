21643d2cc778541bc3368badb933df7b9036c5c6
Vanessa Pereira <vnspereira11@gmail.com>
2023-08-09 18:40:58 -0300

    Add Cors dependency

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ const AppError = require("./utils/AppError");
 
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
