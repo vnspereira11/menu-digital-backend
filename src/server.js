4d0450a973c9e872fbd26f64913c3a5a1ddfb707
Vanessa Pereira <vnspereira11@gmail.com>
2023-08-13 17:54:32 -0300

    Create files route

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 const database = require("./database/sqlite");
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload");
 
 const express = require("express");
 const routes = require("./routes");
@@ -9,6 +10,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 
 database();
